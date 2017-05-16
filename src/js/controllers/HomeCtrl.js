edaf0a108eda237db9ac0aee61dda7ab84e4637b
YuraMolodez <yuramolodez@iMac-Sticker.local>
2017-05-16 15:19:45 +0300

    select is done

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -1,21 +1,19 @@
 angular.module('seoaiApp')
 .controller('HomeCtrl', [
 	'$scope',
-	function($scope) {
-		$scope.message='juds the great';
+	'$http',
+	function($scope, $http) {
+		$scope.selected = null;
+		$scope.websites = [];	
+		$scope.searchWebsites = function(url) {
+			if(url){
+				return $http.get('http://ec2-34-209-22-240.us-west-2.compute.amazonaws.com:8080/api/search/website/' + url)
+				  .then(function(response) {
+				    $scope.websites = response.data	;
+				  }, function(err){
+				  	console.log(err);
+				  });
+			}
+		};
 	}
-])
-.filter('urlFilter', function(){
-	return function(items, props){
-		if(props && props.url){
-			$http({
-			  method: 'GET',
-			  url: 'http://ec2-34-209-22-240.us-west-2.compute.amazonaws.com:8080/api/search/website/' + props.url
-			}).then(function successCallback(response) {
-			    return response;
-			}, function errorCallback(err) {
-			    console.log(err)
-			});	
-		}
-	}
-})
+]);
