d74d421e31e2b1f707c758b11e32ea720360820c
Yehuda Tuvia <ytuvia@gmail.com>
2017-05-16 13:47:07 +0300

    some

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -7,7 +7,15 @@ angular.module('seoaiApp')
 ])
 .filter('urlFilter', function(){
 	return function(items, props){
-		console.log('items', items);
-		console.log('items', props);
+		if(props && props.url){
+			$http({
+			  method: 'GET',
+			  url: 'http://ec2-34-209-22-240.us-west-2.compute.amazonaws.com:8080/api/search/website/' + props.url
+			}).then(function successCallback(response) {
+			    return response;
+			}, function errorCallback(err) {
+			    console.log(err)
+			});	
+		}
 	}
 })
